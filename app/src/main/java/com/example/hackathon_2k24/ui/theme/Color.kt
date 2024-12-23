609b091aa2eea95e773856bf18458b93bd1db4f8
Rameez Akhtar <ramease.work@gmail.com>
2024-12-23 11:33:43 +0530

    Initial commit

---
+++ app/src/main/java/com/example/hackathon_2k24/ui/theme/Color.kt
@@ -0,0 +1,27 @@
+package com.example.hackathon_2k24.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+
+val PrimaryColorLight = Color(0xFFFFA451)
+val PrimaryColorBackgroundLight = Color(0xFFFFFFFF)
+val PrimaryColorTextLight = Color(0xFF000000)
+val PrimaryHighlightColorLight = Color(0xFFF08626)
+
+val PrimaryColorDarkV1 = Color(0xFFFF8C3A)
+val PrimaryColorBackgroundDarkV1 = Color(0xFF121212)
+val PrimaryColorTextDarkV1 = Color(0xFFFFFFFF)
+val PrimaryHighlightColorDarkV1 = Color(0xFFD2691E)
+
+val PrimaryColorDarkV2 = Color(0xFFE66A00)
+val PrimaryColorBackgroundDarkV2 = Color(0xFF1A1A1A)
+val PrimaryColorTextDarkV2 = Color(0xFFE0E0E0)
+val PrimaryHighlightColorDarkV2 = Color(0xFFC35600)
